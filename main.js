6a014af0ea8483057ce58a94dd45782771e321ff
Shyrwinsteel Sia <shyrwinsia@gmail.com>
2024-06-15 15:14:04 +0800

    Added footer and removed drag behavior

--- main.js
+++ main.js
@@ -40,63 +40,66 @@ document.addEventListener('DOMContentLoaded', () => {
   updateCarousel();
 
   // Drag functionality
-  carousel.addEventListener('mousedown', startDrag);
-  carousel.addEventListener('mouseup', endDrag);
-  carousel.addEventListener('mouseleave', endDrag);
-  carousel.addEventListener('mousemove', drag);
-  carousel.addEventListener('touchstart', startDrag);
-  carousel.addEventListener('touchend', endDrag);
-  carousel.addEventListener('touchmove', drag);
+//   carousel.addEventListener('mousedown', startDrag);
+//   carousel.addEventListener('mouseup', endDrag);
+//   carousel.addEventListener('mouseleave', endDrag);
+//   carousel.addEventListener('mousemove', drag);
+//   carousel.addEventListener('touchstart', startDrag);
+//   carousel.addEventListener('touchend', endDrag);
+//   carousel.addEventListener('touchmove', drag);
 
-  function startDrag(event) {
-      isDragging = true;
-      startPos = getPositionX(event);
-      animationID = requestAnimationFrame(animation);
-      carousel.classList.add('dragging');
-  }
+//   function startDrag(event) {
+//       isDragging = true;
+//       startPos = getPositionX(event);
+//       animationID = requestAnimationFrame(animation);
+//       carousel.classList.add('dragging');
+//   }
 
-  function endDrag() {
-      isDragging = false;
-      cancelAnimationFrame(animationID);
+//   function endDrag() {
+//       isDragging = false;
+//       cancelAnimationFrame(animationID);
 
-      const movedBy = currentTranslate - prevTranslate;
+//       const movedBy = currentTranslate - prevTranslate;
 
-      if (movedBy < -50 && currentIndex < images.length - 1) {
-          currentIndex += 1;
-      }
+//       if (movedBy < -50 && currentIndex < images.length - 1) {
+//           currentIndex += 1;
+//       }
 
-      if (movedBy > 50 && currentIndex > 0) {
-          currentIndex -= 1;
-      }
+//       if (movedBy > 50 && currentIndex > 0) {
+//           currentIndex -= 1;
+//       }
 
-      setPositionByIndex();
-      carousel.classList.remove('dragging');
-  }
+//       setPositionByIndex();
+//       carousel.classList.remove('dragging');
+//   }
 
-  function drag(event) {
-      console.log(isDragging)
-      if (isDragging) {
-          const currentPosition = getPositionX(event);
-          currentTranslate = prevTranslate + currentPosition - startPos;
-      }
-  }
+//   function drag(event) {
+//       console.log(isDragging)
+//       if (isDragging) {
+//           const currentPosition = getPositionX(event);
+//           currentTranslate = prevTranslate + currentPosition - startPos;
+//       }
+//   }
 
-  function getPositionX(event) {
-      return event.type.includes('mouse') ? event.clientX : event.touches[0].clientX;
-  }
+//   function getPositionX(event) {
+//       return event.type.includes('mouse') ? event.clientX : event.touches[0].clientX;
+//   }
 
-  function animation() {
-      setSliderPosition();
-      if (isDragging) requestAnimationFrame(animation);
-  }
+//   function animation() {
+//       setSliderPosition();
+//       if (isDragging) requestAnimationFrame(animation);
+//   }
 
-  function setSliderPosition() {
-      carousel.style.transform = `translateX(${currentTranslate}px)`;
-  }
+//   function setSliderPosition() {
+//       carousel.style.transform = `translateX(${currentTranslate}px)`;
+//   }
 
-  function setPositionByIndex() {
-      currentTranslate = currentIndex * -carousel.offsetWidth / 12;
-      prevTranslate = currentTranslate;
-      setSliderPosition();
-  }
+//   function setPositionByIndex() {
+//       currentTranslate = currentIndex * -carousel.offsetWidth / 12;
+//       prevTranslate = currentTranslate;
+//       setSliderPosition();
+//   }
+
+
+  feather.replace();
 });
